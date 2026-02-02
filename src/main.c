adc39ce239e7fb88acffa111de01dd50fcaf96b8
zhaosonggo <zhaosonggo@gmail.com>
2026-02-02 21:57:01 +0800

    light PA8

--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@
 
 int main() {
   GPIO_SetOutput(GPIO_PORT_A, 8, GPIO_SPEED_2MHZ);
+  GPIO_WritePin(GPIO_PORT_A, 8, 1);
   while (1) {
-    GPIO_TogglePin(GPIO_PORT_A, 8);
-    Delay_ms(500);
+    // GPIO_TogglePin(GPIO_PORT_A, 8);
+    // Delay_ms(500);
   }
 }
